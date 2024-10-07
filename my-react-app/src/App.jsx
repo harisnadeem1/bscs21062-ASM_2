de573d6e6e4e5b578acad4c26fb7a91e25b5eacd
Muhammad Haris Nadeem <nadeemharis781@gmail.com>
2024-10-07 22:47:34 +0500

    Admin Panel Components Completed
    5 components that can be useful are created

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import SalesCard from "./components/salescard.jsx";
 import Input from "./components/input.jsx";
 import DropdDown from './components/dropdown-menu.jsx';
+import PrimaryButton from './components/primary-button.jsx';
+import TextArea from './components/TextArea.jsx';
 
 
-
-const Dashboard = () => {
+const Admin = () => {
   return (
     <div >
+
+      <h3 >Admin</h3>
+
       <SalesCard/>
       <Input/>
       <DropdDown/>
+      <TextArea/>
+      <PrimaryButton/>
+
+
     </div>
   );
 };
 
-export default Dashboard;
+export default Admin;
