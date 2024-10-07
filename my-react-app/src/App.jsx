3e99b34a478c589c1d8029b87d54f463f277b53f
Muhammad Haris Nadeem <nadeemharis781@gmail.com>
2024-10-07 23:10:53 +0500

    Argon Social Media Feed
    Left side bar news feed created

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Input from "./components/input.jsx";
 import DropdDown from './components/dropdown-menu.jsx';
 import PrimaryButton from './components/primary-button.jsx';
 import TextArea from './components/TextArea.jsx';
+import LeftSideBar from './components/left-sidebar-button.jsx';
 
 
 const Admin = () => {
@@ -19,8 +20,18 @@ const Admin = () => {
       <PrimaryButton/>
 
 
+
+      <h2>Argon-Social Media</h2>
+      <LeftSideBar/>
+
+
+
     </div>
   );
 };
 
+
+
 export default Admin;
+
+
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './assets/css/bootstrap/bootstrap.min.css'
+import './assets/css/style.css'
+import './assets/css/components.css'
+import './assets/css/media.css'
+import './assets/css/chat.css'
 import Admin from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
